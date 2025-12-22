4e588d2c36bfcd9a2f13670811a73f2fbce6ebc3
Rayn <ryankeeep@gmail.com>
2025-12-22 23:16:25 +0800

    update

--- options.js
+++ options.js
@@ -17,7 +17,7 @@
 	// 限制搜索框宽度
 	searchBoxWidthEl.onchange = function () {
 		let width = this.value;
-		this.value = width < 360 ? 360 : width > 960 ? 960 : width;
+		this.value = width < 360 ? 360 : width > 780 ? 780 : width;
 	}
 
 	// 保存
